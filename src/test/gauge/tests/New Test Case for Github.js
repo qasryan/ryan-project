03499351df8952cf7d9211d0d75509d32f90420d
ryanyackel <ryan.d.yackel@gmail.com>
2017-03-01 13:07:12 -0500

    Test Case Results for New Test Case for Github

---
+++ src/test/gauge/tests/New Test Case for Github.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("New Test Case for Github - should - New Test Case for Github", function() {
+  assert.equal(true, "true");
+});
