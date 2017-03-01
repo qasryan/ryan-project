4cec036ad7b9a51ef6e204050741a743450079b2
ryanyackel <ryan.d.yackel@gmail.com>
2017-03-01 09:35:41 -0500

    Test Case Results for Untitled Test Case

---
+++ src/test/gauge/tests/Untitled Test Case.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("New - should - Test Result", function() {
+  assert.equal(true, "New - should - Test Result");
+});
