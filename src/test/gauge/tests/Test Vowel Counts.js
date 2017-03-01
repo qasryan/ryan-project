a636626bd4de034033a52cccc2e0c5a730206f2d
ryanyackel <ryan.d.yackel@gmail.com>
2017-03-01 09:28:54 -0500

    Test Case Results for Test Vowel Counts

---
+++ src/test/gauge/tests/Test Vowel Counts.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("Vowel counts in single word - should - The word "gauge" has "3" vowels", function() {
+  assert.equal(true, "true");
+});
